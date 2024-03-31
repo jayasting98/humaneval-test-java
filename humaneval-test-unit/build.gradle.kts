5a85dc44d16391e091d0274927d72e565a1d61c3
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-03-31 18:34:44 +0800

    Initialize Gradle project

---
+++ humaneval-test-unit/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
