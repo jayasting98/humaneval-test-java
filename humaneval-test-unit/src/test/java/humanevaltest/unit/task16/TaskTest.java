1903aae277e51bf6cb8cf4d24793f7c3515f0525
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 03:03:36 +0800

    Rewrite task 16

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task16/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task16/TaskTest.java
@@ -1,19 +1,54 @@
 package humanevaltest.unit.task16;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.countDistinctCharacters("") == 0,
-                s.countDistinctCharacters("abcde") == 5,
-                s.countDistinctCharacters("abcde" + "cade" + "CADE") == 5,
-                s.countDistinctCharacters("aaaaAAAAaaaa") == 1,
-                s.countDistinctCharacters("Jerry jERRY JeRRRY") == 5
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testCountDistinctCharacters_emptyString_counts0() {
+        Task task = new Task();
+        String string = "";
+        int expectedCount = 0;
+        int actualCount = task.countDistinctCharacters(string);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testCountDistinctCharacters_allDistinctCharacters_countsLength() {
+        Task task = new Task();
+        String string = "abcde";
+        int expectedCount = 5;
+        int actualCount = task.countDistinctCharacters(string);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testCountDistinctCharacters_repeatedCharacters_countsOnlyDistinctCharacters() {
+        Task task = new Task();
+        String string = "abcde" + "cade" + "CADE";
+        int expectedCount = 5;
+        int actualCount = task.countDistinctCharacters(string);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testCountDistinctCharacters_repeatedCharacterWithDifferentCase_counts1() {
+        Task task = new Task();
+        String string = "aaaaAAAAaaaa";
+        int expectedCount = 1;
+        int actualCount = task.countDistinctCharacters(string);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testCountDistinctCharacters_repeatedCharactersWithSpace_countsOnlyDistinctCharacters() {
+        Task task = new Task();
+        String string = "Jerry jERRY JeRRRY";
+        int expectedCount = 5;
+        int actualCount = task.countDistinctCharacters(string);
+        assertEquals(expectedCount, actualCount);
     }
 }
