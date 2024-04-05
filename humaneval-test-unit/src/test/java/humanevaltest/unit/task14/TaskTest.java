842ae622f185046421b3d11d1e9f3e79a37d5814
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 02:51:44 +0800

    Rewrite task 14

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task14/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task14/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task14;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.allPrefixes("").equals(List.of()),
-                s.allPrefixes("asdfgh").equals(Arrays.asList("a", "as", "asd", "asdf", "asdfg", "asdfgh")),
-                s.allPrefixes("WWW").equals(Arrays.asList("W", "WW", "WWW"))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testAllPrefixes_emptyString_returnsNoPrefixes() {
+        Task task = new Task();
+        String string = "";
+        List<String> expectedPrefixes = List.of();
+        List<String> actualPrefixes = task.allPrefixes(string);
+        assertEquals(expectedPrefixes, actualPrefixes);
+    }
+
+    @Test
+    public void testAllPrefixes_distinctCharacters_returnsPrefixes() {
+        Task task = new Task();
+        String string = "asdfgh";
+        List<String> expectedPrefixes = Arrays.asList("a", "as", "asd", "asdf", "asdfg", "asdfgh");
+        List<String> actualPrefixes = task.allPrefixes(string);
+        assertEquals(expectedPrefixes, actualPrefixes);
+    }
+
+    @Test
+    public void testAllPrefixes_nonDistinctCharacters_returnsPrefixes() {
+        Task task = new Task();
+        String string = "WWW";
+        List<String> expectedPrefixes = Arrays.asList("W", "WW", "WWW");
+        List<String> actualPrefixes = task.allPrefixes(string);
+        assertEquals(expectedPrefixes, actualPrefixes);
     }
 }
