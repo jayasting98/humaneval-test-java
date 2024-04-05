bf13b94c65a8b1a430e6affc80ea43fdf8e9d6a3
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 03:16:26 +0800

    Rewrite task 18

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task18/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task18/TaskTest.java
@@ -1,18 +1,49 @@
 package humanevaltest.unit.task18;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.howManyTimes("", "x") == 0,
-                s.howManyTimes("xyxyxyx", "x") == 4,
-                s.howManyTimes("cacacacac", "cac") == 4,
-                s.howManyTimes("john doe", "john") == 1
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testHowManyTimes_emptyString_counts0() {
+        Task task = new Task();
+        String string = "";
+        String substring = "x";
+        int expectedCount = 0;
+        int actualCount = task.howManyTimes(string, substring);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testHowManyTimes_singleCharacter_countsCorrectly() {
+        Task task = new Task();
+        String string = "xyxyxyx";
+        String substring = "x";
+        int expectedCount = 4;
+        int actualCount = task.howManyTimes(string, substring);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testHowManyTimes_overlappingSubstrings_countsCorrectly() {
+        Task task = new Task();
+        String string = "cacacacac";
+        String substring = "cac";
+        int expectedCount = 4;
+        int actualCount = task.howManyTimes(string, substring);
+        assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void testHowManyTimes_spacedWords_countsCorrectly() {
+        Task task = new Task();
+        String string = "john doe";
+        String substring = "john";
+        int expectedCount = 1;
+        int actualCount = task.howManyTimes(string, substring);
+        assertEquals(expectedCount, actualCount);
     }
 }
