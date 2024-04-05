daf7dff7401dfe0b0e07cd00376b518cf2b074ad
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 03:12:26 +0800

    Rewrite task 17

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task17/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task17/TaskTest.java
@@ -1,19 +1,54 @@
 package humanevaltest.unit.task17;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.parseMusic("").equals(List.of()),
-                s.parseMusic("o o o o").equals(Arrays.asList(4, 4, 4, 4)),
-                s.parseMusic(".| .| .| .|").equals(Arrays.asList(1, 1, 1, 1)),
-                s.parseMusic("o| o| .| .| o o o o").equals(Arrays.asList(2, 2, 1, 1, 4, 4, 4, 4)),
-                s.parseMusic("o| .| o| .| o o| o o|").equals(Arrays.asList(2, 1, 2, 1, 4, 2, 4, 2))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testParseMusic_emptyString_parsesNothing() {
+        Task task = new Task();
+        String string = "";
+        List<Integer> expectedBeats = List.of();
+        List<Integer> actualBeats = task.parseMusic(string);
+        assertEquals(expectedBeats, actualBeats);
+    }
+
+    @Test
+    public void testParseMusic_allWhole_parsesOnly4s() {
+        Task task = new Task();
+        String string = "o o o o";
+        List<Integer> expectedBeats = Arrays.asList(4, 4, 4, 4);
+        List<Integer> actualBeats = task.parseMusic(string);
+        assertEquals(expectedBeats, actualBeats);
+    }
+
+    @Test
+    public void testParseMusic_allQuarters_parsesOnly1s() {
+        Task task = new Task();
+        String string = ".| .| .| .|";
+        List<Integer> expectedBeats = Arrays.asList(1, 1, 1, 1);
+        List<Integer> actualBeats = task.parseMusic(string);
+        assertEquals(expectedBeats, actualBeats);
+    }
+
+    @Test
+    public void testParseMusic_mixed0_parsesCorrectly() {
+        Task task = new Task();
+        String string = "o| o| .| .| o o o o";
+        List<Integer> expectedBeats = Arrays.asList(2, 2, 1, 1, 4, 4, 4, 4);
+        List<Integer> actualBeats = task.parseMusic(string);
+        assertEquals(expectedBeats, actualBeats);
+    }
+
+    @Test
+    public void testParseMusic_mixed1_parsesCorrectly() {
+        Task task = new Task();
+        String string = "o| .| o| .| o o| o o|";
+        List<Integer> expectedBeats = Arrays.asList(2, 1, 2, 1, 4, 2, 4, 2);
+        List<Integer> actualBeats = task.parseMusic(string);
+        assertEquals(expectedBeats, actualBeats);
     }
 }
