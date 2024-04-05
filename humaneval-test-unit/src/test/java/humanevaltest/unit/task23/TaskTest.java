3f732be094858c4bbf61e7b409d8811d98300313
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 03:41:16 +0800

    Rewrite task 23

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task23/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task23/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task23;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.strlen("") == 0,
-                s.strlen("x") == 1,
-                s.strlen("asdasnakj") == 9
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testStrlen_emptyString_measures0() {
+        Task task = new Task();
+        String string = "";
+        int expectedLength = 0;
+        int actualLength = task.strlen(string);
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    public void testStrlen_oneCharacter_measures1() {
+        Task task = new Task();
+        String string = "x";
+        int expectedLength = 1;
+        int actualLength = task.strlen(string);
+        assertEquals(expectedLength, actualLength);
+    }
+
+    @Test
+    public void testStrlen_typicalString_measuresCorrectly() {
+        Task task = new Task();
+        String string = "asdasnakj";
+        int expectedLength = 9;
+        int actualLength = task.strlen(string);
+        assertEquals(expectedLength, actualLength);
     }
 }
