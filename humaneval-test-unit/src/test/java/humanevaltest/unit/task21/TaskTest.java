37772a0c01371418ab19d477ce2d5af7dc4b5285
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 03:34:29 +0800

    Rewrite task 21

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task21/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task21/TaskTest.java
@@ -1,19 +1,54 @@
 package humanevaltest.unit.task21;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.rescaleToUnit(new ArrayList<>(Arrays.asList(2.0, 49.9))).equals(Arrays.asList(0.0, 1.0)),
-                s.rescaleToUnit(new ArrayList<>(Arrays.asList(100.0, 49.9))).equals(Arrays.asList(1.0, 0.0)),
-                s.rescaleToUnit(new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0))).equals(Arrays.asList(0.0, 0.25, 0.5, 0.75, 1.0)),
-                s.rescaleToUnit(new ArrayList<>(Arrays.asList(2.0, 1.0, 5.0, 3.0, 4.0))).equals(Arrays.asList(0.25, 0.0, 1.0, 0.5, 0.75)),
-                s.rescaleToUnit(new ArrayList<>(Arrays.asList(12.0, 11.0, 15.0, 13.0, 14.0))).equals(Arrays.asList(0.25, 0.0, 1.0, 0.5, 0.75))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testRescaleToUnit_twoIncreasingNumbers_returns0And1() {
+        Task task = new Task();
+        List<Double> numbers = Arrays.asList(2.0, 49.9);
+        List<Double> expectedRescaledNumbers = Arrays.asList(0.0, 1.0);
+        List<Double> actualRescaledNumbers = task.rescaleToUnit(numbers);
+        assertEquals(expectedRescaledNumbers, actualRescaledNumbers);
+    }
+
+    @Test
+    public void testRescaleToUnit_twoDecreasingNumbers_returns1And0() {
+        Task task = new Task();
+        List<Double> numbers = Arrays.asList(100.0, 49.9);
+        List<Double> expectedRescaledNumbers = Arrays.asList(1.0, 0.0);
+        List<Double> actualRescaledNumbers = task.rescaleToUnit(numbers);
+        assertEquals(expectedRescaledNumbers, actualRescaledNumbers);
+    }
+
+    @Test
+    public void testRescaleToUnit_moreThanTwoIncreasingNumbers_returnsRescaledIncreasingNumbers() {
+        Task task = new Task();
+        List<Double> numbers = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
+        List<Double> expectedRescaledNumbers = Arrays.asList(0.0, 0.25, 0.5, 0.75, 1.0);
+        List<Double> actualRescaledNumbers = task.rescaleToUnit(numbers);
+        assertEquals(expectedRescaledNumbers, actualRescaledNumbers);
+    }
+
+    @Test
+    public void testRescaleToUnit_moreThanTwoUnsortedNumbers_returnsRescaledNumbersInSameOrder() {
+        Task task = new Task();
+        List<Double> numbers = Arrays.asList(2.0, 1.0, 5.0, 3.0, 4.0);
+        List<Double> expectedRescaledNumbers = Arrays.asList(0.25, 0.0, 1.0, 0.5, 0.75);
+        List<Double> actualRescaledNumbers = task.rescaleToUnit(numbers);
+        assertEquals(expectedRescaledNumbers, actualRescaledNumbers);
+    }
+
+    @Test
+    public void testRescaleToUnit_numbersWithArbitraryMinimum_returnsRescaledNumbersInSameOrder() {
+        Task task = new Task();
+        List<Double> numbers = Arrays.asList(12.0, 11.0, 15.0, 13.0, 14.0);
+        List<Double> expectedRescaledNumbers = Arrays.asList(0.25, 0.0, 1.0, 0.5, 0.75);
+        List<Double> actualRescaledNumbers = task.rescaleToUnit(numbers);
+        assertEquals(expectedRescaledNumbers, actualRescaledNumbers);
     }
 }
