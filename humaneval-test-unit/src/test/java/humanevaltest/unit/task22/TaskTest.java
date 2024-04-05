0d931b3fa6cb05b9768bbc7b117295a869f14fa6
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 03:38:27 +0800

    Rewrite task 22

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task22/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task22/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task22;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.filterIntegers(new ArrayList<>(List.of())).equals(List.of()),
-                s.filterIntegers(new ArrayList<>(Arrays.asList(4, Map.of(), List.of(), 23.2, 9, "adasd"))).equals(Arrays.asList(4, 9)),
-                s.filterIntegers(new ArrayList<>(Arrays.asList(3, 'c', 3, 3, 'a', 'b'))).equals(Arrays.asList(3, 3, 3))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testFilterIntegers_emptyValues_filtersNothing() {
+        Task task = new Task();
+        List<Object> values = List.of();
+        List<Integer> expectedIntegers = List.of();
+        List<Integer> actualIntegers = task.filterIntegers(values);
+        assertEquals(expectedIntegers, actualIntegers);
+    }
+
+    @Test
+    public void testFilterIntegers_multipleTypes_filtersIntegers() {
+        Task task = new Task();
+        List<Object> values = Arrays.asList(4, Map.of(), List.of(), 23.2, 9, "adasd");
+        List<Integer> expectedIntegers = Arrays.asList(4, 9);
+        List<Integer> actualIntegers = task.filterIntegers(values);
+        assertEquals(expectedIntegers, actualIntegers);
+    }
+
+    @Test
+    public void testFilterIntegers_duplicateIntegers_filtersIntegers() {
+        Task task = new Task();
+        List<Object> values = Arrays.asList(3, 'c', 3, 3, 'a', 'b');
+        List<Integer> expectedIntegers = Arrays.asList(3, 3, 3);
+        List<Integer> actualIntegers = task.filterIntegers(values);
+        assertEquals(expectedIntegers, actualIntegers);
     }
 }
