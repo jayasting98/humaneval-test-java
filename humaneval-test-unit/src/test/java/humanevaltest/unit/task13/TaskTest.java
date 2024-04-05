3d9ed0fb4736a36895067cd4cb5adf2acf0f20c9
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 02:48:10 +0800

    Rewrite task 13

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task13/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task13/TaskTest.java
@@ -1,18 +1,49 @@
 package humanevaltest.unit.task13;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.greatestCommonDivisor(3, 7) == 1,
-                s.greatestCommonDivisor(10, 15) == 5,
-                s.greatestCommonDivisor(49, 14) == 7,
-                s.greatestCommonDivisor(144, 60) == 12
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testGreatestCommonDivisor_primes_returns1() {
+        Task task = new Task();
+        int a = 3;
+        int b = 7;
+        int expectedDivisor = 1;
+        int actualDivisor = task.greatestCommonDivisor(a, b);
+        assertEquals(expectedDivisor, actualDivisor);
+    }
+
+    @Test
+    public void testGreatestCommonDivisor_nonCoPrimes0_returnsMoreThan1() {
+        Task task = new Task();
+        int a = 10;
+        int b = 15;
+        int expectedDivisor = 5;
+        int actualDivisor = task.greatestCommonDivisor(a, b);
+        assertEquals(expectedDivisor, actualDivisor);
+    }
+
+    @Test
+    public void testGreatestCommonDivisor_nonCoPrimes1_returnsMoreThan1() {
+        Task task = new Task();
+        int a = 49;
+        int b = 14;
+        int expectedDivisor = 7;
+        int actualDivisor = task.greatestCommonDivisor(a, b);
+        assertEquals(expectedDivisor, actualDivisor);
+    }
+
+    @Test
+    public void testGreatestCommonDivisor_nonCoPrimes2_returnsMoreThan1() {
+        Task task = new Task();
+        int a = 144;
+        int b = 60;
+        int expectedDivisor = 12;
+        int actualDivisor = task.greatestCommonDivisor(a, b);
+        assertEquals(expectedDivisor, actualDivisor);
     }
 }
