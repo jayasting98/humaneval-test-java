1122ff9261dbcfeb0d51524a6d0728e2cccfa6ed
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 02:55:31 +0800

    Rewrite task 15

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task15/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task15/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task15;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.stringSequence(0).equals("0"),
-                s.stringSequence(3).equals("0 1 2 3"),
-                s.stringSequence(10).equals("0 1 2 3 4 5 6 7 8 9 10")
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testStringSequence_0_only0() {
+        Task task = new Task();
+        int n = 0;
+        String expectedSequence = "0";
+        String actualSequence = task.stringSequence(n);
+        assertEquals(expectedSequence, actualSequence);
+    }
+
+    @Test
+    public void testStringSequence_3_0to3() {
+        Task task = new Task();
+        int n = 3;
+        String expectedSequence = "0 1 2 3";
+        String actualSequence = task.stringSequence(n);
+        assertEquals(expectedSequence, actualSequence);
+    }
+
+    @Test
+    public void testStringSequence_10_0to10() {
+        Task task = new Task();
+        int n = 10;
+        String expectedSequence = "0 1 2 3 4 5 6 7 8 9 10";
+        String actualSequence = task.stringSequence(n);
+        assertEquals(expectedSequence, actualSequence);
     }
 }
