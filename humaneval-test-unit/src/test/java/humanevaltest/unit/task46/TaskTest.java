911654b53801958119ba19dc1df57338b008e4e0
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 22:57:53 +0800

    Rewrite task 46

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task46/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task46/TaskTest.java
@@ -1,18 +1,18 @@
 package humanevaltest.unit.task46;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.fib4(5) == 4,
-                s.fib4(8) == 28,
-                s.fib4(10) == 104,
-                s.fib4(12) == 386
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testFib4_typicalCase_calculatesCorrectly() {
+        Task task = new Task();
+        assertEquals(4, task.fib4(5));
+        assertEquals(28, task.fib4(8));
+        assertEquals(104, task.fib4(10));
+        assertEquals(386, task.fib4(12));
     }
 }
