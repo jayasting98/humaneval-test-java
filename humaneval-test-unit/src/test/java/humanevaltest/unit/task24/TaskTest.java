af1d2f1ec0098c42d0642ae0a63e307730b9835f
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 13:23:43 +0800

    Rewrite task 24

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task24/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task24/TaskTest.java
@@ -1,19 +1,54 @@
 package humanevaltest.unit.task24;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.largestDivisor(3) == 1,
-                s.largestDivisor(7) == 1,
-                s.largestDivisor(10) == 5,
-                s.largestDivisor(100) == 50,
-                s.largestDivisor(49) == 7
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testLargestDivisor_prime0_finds1() {
+        Task task = new Task();
+        int n = 3;
+        int expectedLargestDivisor = 1;
+        int actualLargestDivisor = task.largestDivisor(n);
+        assertEquals(expectedLargestDivisor, actualLargestDivisor);
+    }
+
+    @Test
+    public void testLargestDivisor_prime1_finds1() {
+        Task task = new Task();
+        int n = 7;
+        int expectedLargestDivisor = 1;
+        int actualLargestDivisor = task.largestDivisor(n);
+        assertEquals(expectedLargestDivisor, actualLargestDivisor);
+    }
+
+    @Test
+    public void testLargestDivisor_nonPrime0_findsLargestDivisor() {
+        Task task = new Task();
+        int n = 10;
+        int expectedLargestDivisor = 5;
+        int actualLargestDivisor = task.largestDivisor(n);
+        assertEquals(expectedLargestDivisor, actualLargestDivisor);
+    }
+
+    @Test
+    public void testLargestDivisor_nonPrime1_findsLargestDivisor() {
+        Task task = new Task();
+        int n = 100;
+        int expectedLargestDivisor = 50;
+        int actualLargestDivisor = task.largestDivisor(n);
+        assertEquals(expectedLargestDivisor, actualLargestDivisor);
+    }
+
+    @Test
+    public void testLargestDivisor_nonPrime2_findsLargestDivisor() {
+        Task task = new Task();
+        int n = 49;
+        int expectedLargestDivisor = 7;
+        int actualLargestDivisor = task.largestDivisor(n);
+        assertEquals(expectedLargestDivisor, actualLargestDivisor);
     }
 }
