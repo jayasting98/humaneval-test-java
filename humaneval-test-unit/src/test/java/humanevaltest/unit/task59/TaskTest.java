5459e68a28f74b03478232fbda08e4e587461478
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-07 02:16:51 +0800

    Rewrite task 59

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task59/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task59/TaskTest.java
@@ -1,19 +1,54 @@
 package humanevaltest.unit.task59;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.largestPrimeFactor(15) == 5,
-                s.largestPrimeFactor(27) == 3,
-                s.largestPrimeFactor(63) == 7,
-                s.largestPrimeFactor(330) == 11,
-                s.largestPrimeFactor(13195) == 29
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testLargestPrimeFactor_distinctPrimeFactors0_returnsLargestPrimeFactor() {
+        Task task = new Task();
+        int n = 15;
+        int expectedLargestPrimeFactor = 5;
+        int actualLargestPrimeFactor = task.largestPrimeFactor(n);
+        assertEquals(expectedLargestPrimeFactor, actualLargestPrimeFactor);
+    }
+
+    @Test
+    public void testLargestPrimeFactor_cubedPrime_returnsPrime() {
+        Task task = new Task();
+        int n = 27;
+        int expectedLargestPrimeFactor = 3;
+        int actualLargestPrimeFactor = task.largestPrimeFactor(n);
+        assertEquals(expectedLargestPrimeFactor, actualLargestPrimeFactor);
+    }
+
+    @Test
+    public void testLargestPrimeFactor_squaredSmallerPrime_returnsLargestPrimeFactor() {
+        Task task = new Task();
+        int n = 63;
+        int expectedLargestPrimeFactor = 7;
+        int actualLargestPrimeFactor = task.largestPrimeFactor(n);
+        assertEquals(expectedLargestPrimeFactor, actualLargestPrimeFactor);
+    }
+
+    @Test
+    public void testLargestPrimeFactor_distinctPrimeFactors1_returnsLargestPrimeFactor() {
+        Task task = new Task();
+        int n = 330;
+        int expectedLargestPrimeFactor = 11;
+        int actualLargestPrimeFactor = task.largestPrimeFactor(n);
+        assertEquals(expectedLargestPrimeFactor, actualLargestPrimeFactor);
+    }
+
+    @Test
+    public void testLargestPrimeFactor_distinctPrimeFactors2_returnsLargestPrimeFactor() {
+        Task task = new Task();
+        int n = 13195;
+        int expectedLargestPrimeFactor = 29;
+        int actualLargestPrimeFactor = task.largestPrimeFactor(n);
+        assertEquals(expectedLargestPrimeFactor, actualLargestPrimeFactor);
     }
 }
