b3ba6f09dd8bfe14f8ff3ae11b5897ca8ac44df9
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 14:09:35 +0800

    Rewrite task 31

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task31/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task31/TaskTest.java
@@ -1,27 +1,37 @@
 package humanevaltest.unit.task31;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                !s.isPrime(6),
-                s.isPrime(101),
-                s.isPrime(11),
-                s.isPrime(13441),
-                s.isPrime(61),
-                !s.isPrime(4),
-                !s.isPrime(1),
-                s.isPrime(5),
-                s.isPrime(11),
-                s.isPrime(17),
-                !s.isPrime(5 * 17),
-                !s.isPrime(11 * 7),
-                !s.isPrime(13441 * 19)
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testIsPrime_one_returnsFalse() {
+        Task task = new Task();
+        assertFalse(task.isPrime(1));
+    }
+
+    @Test
+    public void testIsPrime_compositeNumber_returnsFalse() {
+        Task task = new Task();
+        assertFalse(task.isPrime(6));
+        assertFalse(task.isPrime(4));
+        assertFalse(task.isPrime(5 * 17));
+        assertFalse(task.isPrime(11 * 7));
+        assertFalse(task.isPrime(13441 * 19));
+    }
+
+    @Test
+    public void testIsPrime_primeNumber_returnsTrue() {
+        Task task = new Task();
+        assertTrue(task.isPrime(101));
+        assertTrue(task.isPrime(11));
+        assertTrue(task.isPrime(13441));
+        assertTrue(task.isPrime(61));
+        assertTrue(task.isPrime(5));
+        assertTrue(task.isPrime(11));
+        assertTrue(task.isPrime(17));
     }
 }
