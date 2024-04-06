1c2e060bb40bbcbf85eb689f6591eb6ffb739837
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 23:06:25 +0800

    Rewrite task 48

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task48/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task48/TaskTest.java
@@ -1,21 +1,58 @@
 package humanevaltest.unit.task48;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.isPalindrome(""),
-                s.isPalindrome("aba"),
-                s.isPalindrome("aaaaa"),
-                !s.isPalindrome("zbcd"),
-                s.isPalindrome("xywyx"),
-                !s.isPalindrome("xywyz"),
-                !s.isPalindrome("xywzx")
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testIsPalindrome_emptyString_returnsTrue() {
+        Task task = new Task();
+        String text = "";
+        assertTrue(task.isPalindrome(text));
+    }
+
+    @Test
+    public void testIsPalindrome_palindrome_returnsTrue() {
+        Task task = new Task();
+        String text = "aba";
+        assertTrue(task.isPalindrome(text));
+    }
+
+    @Test
+    public void testIsPalindrome_allSameCharacter_returnsTrue() {
+        Task task = new Task();
+        String text = "aaaaa";
+        assertTrue(task.isPalindrome(text));
+    }
+
+    @Test
+    public void testIsPalindrome_nonPalindrome_returnsFalse() {
+        Task task = new Task();
+        String text = "zbcd";
+        assertFalse(task.isPalindrome(text));
+    }
+
+    @Test
+    public void testIsPalindrome_palindromeWithMoreCharacters_returnsTrue() {
+        Task task = new Task();
+        String text = "xywyx";
+        assertTrue(task.isPalindrome(text));
+    }
+
+    @Test
+    public void testIsPalindrome_almostPlaindrome0_returnsFalse() {
+        Task task = new Task();
+        String text = "xywyz";
+        assertFalse(task.isPalindrome(text));
+    }
+
+    @Test
+    public void testIsPalindrome_almostPlaindrome1_returnsFalse() {
+        Task task = new Task();
+        String text = "xywzx";
+        assertFalse(task.isPalindrome(text));
     }
 }
