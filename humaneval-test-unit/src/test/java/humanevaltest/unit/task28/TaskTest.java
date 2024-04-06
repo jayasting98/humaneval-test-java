1558a4719b7128443f5f63ae30e9553df954e35a
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 13:48:16 +0800

    Rewrite task 28

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task28/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task28/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task28;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                Objects.equals(s.concatenate(new ArrayList<>(List.of())), ""),
-                Objects.equals(s.concatenate(new ArrayList<>(Arrays.asList("x", "y", "z"))), "xyz"),
-                Objects.equals(s.concatenate(new ArrayList<>(Arrays.asList("x", "y", "z", "w", "k"))), "xyzwk")
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testConcatenate_emptyList_concatenatesNothing() {
+        Task task = new Task();
+        List<String> strings = List.of();
+        String expectedConcatenation = "";
+        String actualConcatenation = task.concatenate(strings);
+        assertEquals(expectedConcatenation, actualConcatenation);
+    }
+
+    @Test
+    public void testConcatenate_typicalStrings0_concatenatesStrings() {
+        Task task = new Task();
+        List<String> strings = Arrays.asList("x", "y", "z");
+        String expectedConcatenation = "xyz";
+        String actualConcatenation = task.concatenate(strings);
+        assertEquals(expectedConcatenation, actualConcatenation);
+    }
+
+    @Test
+    public void testConcatenate_typicalStrings1_concatenatesStrings() {
+        Task task = new Task();
+        List<String> strings = Arrays.asList("x", "y", "z", "w", "k");
+        String expectedConcatenation = "xyzwk";
+        String actualConcatenation = task.concatenate(strings);
+        assertEquals(expectedConcatenation, actualConcatenation);
     }
 }
