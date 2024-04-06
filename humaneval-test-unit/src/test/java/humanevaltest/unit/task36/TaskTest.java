3c3c3f7709cb71c89c7b1cb41dffc351d0ff9b20
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 19:18:16 +0800

    Rewrite task 36

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task36/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task36/TaskTest.java
@@ -1,22 +1,27 @@
 package humanevaltest.unit.task36;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.fizzBuzz(50) == 0,
-                s.fizzBuzz(78) == 2,
-                s.fizzBuzz(79) == 3,
-                s.fizzBuzz(100) == 3,
-                s.fizzBuzz(200) == 6,
-                s.fizzBuzz(4000) == 192,
-                s.fizzBuzz(10000) == 639,
-                s.fizzBuzz(100000) == 8026
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testFizzBuzz_noMultiplesOf11Or13WithDigit7_counts0() {
+        Task task = new Task();
+        assertEquals(0, task.fizzBuzz(50));
+    }
+
+    @Test
+    public void testFizzBuzz_someMultiplesOf11Or13WithDigit7_countsCorrectly() {
+        Task task = new Task();
+        assertEquals(2, task.fizzBuzz(78));
+        assertEquals(3, task.fizzBuzz(79));
+        assertEquals(3, task.fizzBuzz(100));
+        assertEquals(6, task.fizzBuzz(200));
+        assertEquals(192, task.fizzBuzz(4000));
+        assertEquals(639, task.fizzBuzz(10000));
+        assertEquals(8026, task.fizzBuzz(100000));
     }
 }
