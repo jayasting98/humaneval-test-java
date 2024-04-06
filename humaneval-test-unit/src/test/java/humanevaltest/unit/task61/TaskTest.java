723ebd1cc89ccfa9a86049c66882c210d879f22d
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-07 02:29:52 +0800

    Rewrite task 61

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task61/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task61/TaskTest.java
@@ -1,26 +1,93 @@
 package humanevaltest.unit.task61;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.correctBracketing("()"),
-                s.correctBracketing("(()())"),
-                s.correctBracketing("()()(()())()"),
-                s.correctBracketing("()()((()()())())(()()(()))"),
-                !s.correctBracketing("((()())))"),
-                !s.correctBracketing(")(()"),
-                !s.correctBracketing("("),
-                !s.correctBracketing("(((("),
-                !s.correctBracketing(")"),
-                !s.correctBracketing("(()"),
-                !s.correctBracketing("()()(()())())(()"),
-                !s.correctBracketing("()()(()())()))()")
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testCorrectBracketing_balanced0_returnsTrue() {
+        Task task = new Task();
+        String brackets = "()";
+        assertTrue(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_balanced1_returnsTrue() {
+        Task task = new Task();
+        String brackets = "(()())";
+        assertTrue(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_balanced2_returnsTrue() {
+        Task task = new Task();
+        String brackets = "()()(()())()";
+        assertTrue(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_balanced3_returnsTrue() {
+        Task task = new Task();
+        String brackets = "()()((()()())())(()()(()))";
+        assertTrue(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraClosingBracket0_returnsFalse() {
+        Task task = new Task();
+        String brackets = "((()())))";
+        assertFalse(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraClosingBracket1_returnsFalse() {
+        Task task = new Task();
+        String brackets = ")(()";
+        assertFalse(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraOpeningBrackets0_returnsFalse() {
+        Task task = new Task();
+        String brackets = "(";
+        assertFalse(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraOpeningBrackets1_returnsFalse() {
+        Task task = new Task();
+        String brackets = "((((";
+        assertFalse(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraClosingBracket2_returnsFalse() {
+        Task task = new Task();
+        String brackets = ")";
+        assertFalse(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraOpeningBrackets2_returnsFalse() {
+        Task task = new Task();
+        String brackets = "(()";
+        assertFalse(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraClosingBracket3_returnsFalse() {
+        Task task = new Task();
+        String brackets = "()()(()())())(()";
+        assertFalse(task.correctBracketing(brackets));
+    }
+
+    @Test
+    public void testCorrectBracketing_extraClosingBracket4_returnsFalse() {
+        Task task = new Task();
+        String brackets = "()()(()())()))()";
+        assertFalse(task.correctBracketing(brackets));
     }
 }
