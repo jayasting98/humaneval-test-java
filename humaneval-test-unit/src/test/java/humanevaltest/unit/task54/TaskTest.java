a3b8eebf32a404c20cb39f54e7fdf6ac51c75faa
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-07 01:48:51 +0800

    Rewrite task 54

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task54/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task54/TaskTest.java
@@ -1,21 +1,65 @@
 package humanevaltest.unit.task54;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.sameChars("eabcdzzzz", "dddzzzzzzzddeddabc"),
-                s.sameChars("abcd", "dddddddabc"),
-                s.sameChars("dddddddabc", "abcd"),
-                !s.sameChars("eabcd", "dddddddabc"),
-                !s.sameChars("abcd", "dddddddabcf"),
-                !s.sameChars("eabcdzzzz", "dddzzzzzzzddddabc"),
-                !s.sameChars("aabb", "aaccc")
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testSameChars_sameCharacters0_returnsTrue() {
+        Task task = new Task();
+        String string1 = "eabcdzzzz";
+        String string2 = "dddzzzzzzzddeddabc";
+        assertTrue(task.sameChars(string1, string2));
+    }
+
+    @Test
+    public void testSameChars_sameCharacters1_returnsTrue() {
+        Task task = new Task();
+        String string1 = "abcd";
+        String string2 = "dddddddabc";
+        assertTrue(task.sameChars(string1, string2));
+    }
+
+    @Test
+    public void testSameChars_sameCharacters2_returnsTrue() {
+        Task task = new Task();
+        String string1 = "dddddddabc";
+        String string2 = "abcd";
+        assertTrue(task.sameChars(string1, string2));
+    }
+
+    @Test
+    public void testSameChars_differentCharacters0_returnsFalse() {
+        Task task = new Task();
+        String string1 = "eabcd";
+        String string2 = "dddddddabc";
+        assertFalse(task.sameChars(string1, string2));
+    }
+
+    @Test
+    public void testSameChars_differentCharacters1_returnsFalse() {
+        Task task = new Task();
+        String string1 = "abcd";
+        String string2 = "dddddddabcf";
+        assertFalse(task.sameChars(string1, string2));
+    }
+
+    @Test
+    public void testSameChars_differentCharacters2_returnsFalse() {
+        Task task = new Task();
+        String string1 = "eabcdzzzz";
+        String string2 = "dddzzzzzzzddddabc";
+        assertFalse(task.sameChars(string1, string2));
+    }
+
+    @Test
+    public void testSameChars_differentCharacters3_returnsFalse() {
+        Task task = new Task();
+        String string1 = "aabb";
+        String string2 = "aaccc";
+        assertFalse(task.sameChars(string1, string2));
     }
 }
