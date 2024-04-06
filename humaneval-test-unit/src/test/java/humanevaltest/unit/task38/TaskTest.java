7fb4c5ad907cde04dd0c12ec38acca783c042340
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 19:30:53 +0800

    Rewrite task 38

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task38/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task38/TaskTest.java
@@ -1,25 +1,30 @@
 package humanevaltest.unit.task38;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
+
+import org.junit.jupiter.api.Test;
 
 public class TaskTest {
     static char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     static Random rand = new Random(42);
-    public static String random_string(int length) {
+
+    public static String randomString(int length) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
             sb.append(letters[rand.nextInt(26)]);
         }
         return sb.toString();
     }
-    public static void main(String[] args) {
-        Task s = new Task();
+
+    @Test
+    public void testEncodeCyclic_randomStrings_encodesCorrectly() {
+        Task task = new Task();
         for (int i = 0; i < 100; i++) {
-            String str = random_string(rand.nextInt(10) + 10);
-            String encode_str = s.encodeCyclic(str);
-            if (!s.decodeCyclic(encode_str).equals(str)) {
-                throw new AssertionError();
-            }
+            String str = randomString(rand.nextInt(10) + 10);
+            String encodedStr = task.encodeCyclic(str);
+            assertEquals(str, task.decodeCyclic(encodedStr));
         }
     }
 }
