a747b74f6d85dec9822cf6b1465474b82d4e8b35
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 23:02:00 +0800

    Rewrite task 47

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task47/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task47/TaskTest.java
@@ -1,18 +1,45 @@
 package humanevaltest.unit.task47;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.median(new ArrayList<>(Arrays.asList(3, 1, 2, 4, 5))) == 3,
-                s.median(new ArrayList<>(Arrays.asList(-10, 4, 6, 1000, 10, 20))) == 8.0,
-                s.median(new ArrayList<>(Arrays.asList(5))) == 5,
-                s.median(new ArrayList<>(Arrays.asList(6, 5))) == 5.5
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testMedian_oddNumberOfElementsWithMoreThanOneOnEachHalf_calculatesCorrectly() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(3, 1, 2, 4, 5);
+        double expectedMedian = 3.0;
+        double actualMedian = task.median(l);
+        assertEquals(expectedMedian, actualMedian);
+    }
+
+    @Test
+    public void testMedian_evenNumberOfElementsWithMoreThanOneOnEachHalf_calculatesCorrectly() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(-10, 4, 6, 1000, 10, 20);
+        double expectedMedian = 8.0;
+        double actualMedian = task.median(l);
+        assertEquals(expectedMedian, actualMedian);
+    }
+
+    @Test
+    public void testMedian_oddNumberOfElementsWithAtMostThanOneOnEachHalf_calculatesCorrectly() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5);
+        double expectedMedian = 5.0;
+        double actualMedian = task.median(l);
+        assertEquals(expectedMedian, actualMedian);
+    }
+
+    @Test
+    public void testMedian_evenNumberOfElementsWithAtMostThanOneOnEachHalf_calculatesCorrectly() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(6, 5);
+        double expectedMedian = 5.5;
+        double actualMedian = task.median(l);
+        assertEquals(expectedMedian, actualMedian);
     }
 }
