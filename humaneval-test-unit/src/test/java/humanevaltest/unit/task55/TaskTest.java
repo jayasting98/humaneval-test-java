5e5d60d1c7ef764fad228ad9f64c0a58b6ae8fb4
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-07 01:52:47 +0800

    Rewrite task 55

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task55/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task55/TaskTest.java
@@ -1,19 +1,54 @@
 package humanevaltest.unit.task55;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.fib(10) == 55,
-                s.fib(1) == 1,
-                s.fib(8) == 21,
-                s.fib(11) == 89,
-                s.fib(12) == 144
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testFib_nLargerThanOne0_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 10;
+        int expectedFibonacciNumber = 55;
+        int actualFibonacciNumber = task.fib(n);
+        assertEquals(expectedFibonacciNumber, actualFibonacciNumber);
+    }
+
+    @Test
+    public void testFib_nEqualsOne_calculates1() {
+        Task task = new Task();
+        int n = 1;
+        int expectedFibonacciNumber = 1;
+        int actualFibonacciNumber = task.fib(n);
+        assertEquals(expectedFibonacciNumber, actualFibonacciNumber);
+    }
+
+    @Test
+    public void testFib_nLargerThanOne1_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 8;
+        int expectedFibonacciNumber = 21;
+        int actualFibonacciNumber = task.fib(n);
+        assertEquals(expectedFibonacciNumber, actualFibonacciNumber);
+    }
+
+    @Test
+    public void testFib_nLargerThanOne2_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 11;
+        int expectedFibonacciNumber = 89;
+        int actualFibonacciNumber = task.fib(n);
+        assertEquals(expectedFibonacciNumber, actualFibonacciNumber);
+    }
+
+    @Test
+    public void testFib_nLargerThanOne3_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 12;
+        int expectedFibonacciNumber = 144;
+        int actualFibonacciNumber = task.fib(n);
+        assertEquals(expectedFibonacciNumber, actualFibonacciNumber);
     }
 }
