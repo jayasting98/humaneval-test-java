6ff8e10fbeb2dd1efb59a4b26dca486a779aaea3
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 19:24:30 +0800

    Rewrite task 37

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task37/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task37/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task37;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.sortEven(new ArrayList<>(Arrays.asList(1, 2, 3))).equals(Arrays.asList(1, 2, 3)),
-                s.sortEven(new ArrayList<>(Arrays.asList(5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10))).equals(Arrays.asList(-10, 3, -5, 2, -3, 3, 5, 0, 9, 1, 123)),
-                s.sortEven(new ArrayList<>(Arrays.asList(5, 8, -12, 4, 23, 2, 3, 11, 12, -10))).equals(Arrays.asList(-12, 8, 3, 4, 5, 2, 12, 11, 23, -10))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testSortEven_sortedEvenIndices_returnsSameOrder() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(1, 2, 3);
+        List<Integer> expectedSortedEvenIndices = Arrays.asList(1, 2, 3);
+        List<Integer> actualSortedEvenIndices = task.sortEven(l);
+        assertEquals(expectedSortedEvenIndices, actualSortedEvenIndices);
+    }
+
+    @Test
+    public void testSortEven_unsortedEvenIndicesWithOddLength_returnsWithSortedEvenIndices() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10);
+        List<Integer> expectedSortedEvenIndices = Arrays.asList(-10, 3, -5, 2, -3, 3, 5, 0, 9, 1, 123);
+        List<Integer> actualSortedEvenIndices = task.sortEven(l);
+        assertEquals(expectedSortedEvenIndices, actualSortedEvenIndices);
+    }
+
+    @Test
+    public void testSortEven_unsortedEvenIndicesWithEvenLength_returnsWithSortedEvenIndices() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 8, -12, 4, 23, 2, 3, 11, 12, -10);
+        List<Integer> expectedSortedEvenIndices = Arrays.asList(-12, 8, 3, 4, 5, 2, 12, 11, 23, -10);
+        List<Integer> actualSortedEvenIndices = task.sortEven(l);
+        assertEquals(expectedSortedEvenIndices, actualSortedEvenIndices);
     }
 }
