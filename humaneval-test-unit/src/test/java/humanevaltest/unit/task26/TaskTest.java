ac7bc26b96a9c959e5418b36f7e83ed8211e1a8a
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 13:39:39 +0800

    Rewrite task 26

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task26/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task26/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task26;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.removeDuplicates(new ArrayList<>(List.of())).equals(List.of()),
-                s.removeDuplicates(new ArrayList<>(Arrays.asList(1, 2, 3, 4))).equals(Arrays.asList(1, 2, 3, 4)),
-                s.removeDuplicates(new ArrayList<>(Arrays.asList(1, 2, 3, 2, 4, 3, 5))).equals(Arrays.asList(1, 4, 5))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testRemoveDuplicates_emptyList_returnsSameEmptyList() {
+        Task task = new Task();
+        List<Integer> numbers = List.of();
+        List<Integer> expectedUniqueNumbers = List.of();
+        List<Integer> actualUniqueNumbers = task.removeDuplicates(numbers);
+        assertEquals(expectedUniqueNumbers, actualUniqueNumbers);
+    }
+
+    @Test
+    public void testRemoveDuplicates_uniqueNumbers_returnsSameUniqueNumbers() {
+        Task task = new Task();
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        List<Integer> expectedUniqueNumbers = Arrays.asList(1, 2, 3, 4);
+        List<Integer> actualUniqueNumbers = task.removeDuplicates(numbers);
+        assertEquals(expectedUniqueNumbers, actualUniqueNumbers);
+    }
+
+    @Test
+    public void testRemoveDuplicates_duplicateNumbers_returnsUniqueNumbers() {
+        Task task = new Task();
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 2, 4, 3, 5);
+        List<Integer> expectedUniqueNumbers = Arrays.asList(1, 4, 5);
+        List<Integer> actualUniqueNumbers = task.removeDuplicates(numbers);
+        assertEquals(expectedUniqueNumbers, actualUniqueNumbers);
     }
 }
