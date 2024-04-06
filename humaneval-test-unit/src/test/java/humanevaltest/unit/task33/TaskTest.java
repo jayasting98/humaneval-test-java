5bc24c54e00b2bd251ad39f10fdd9997b339b4ed
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 14:35:31 +0800

    Rewrite task 33

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task33/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task33/TaskTest.java
@@ -1,18 +1,45 @@
 package humanevaltest.unit.task33;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.sortThird(new ArrayList<>(Arrays.asList(5, 6, 3, 4, 8, 9, 2))).equals(Arrays.asList(2, 6, 3, 4, 8, 9, 5)),
-                s.sortThird(new ArrayList<>(Arrays.asList(5, 8, 3, 4, 6, 9, 2))).equals(Arrays.asList(2, 8, 3, 4, 6, 9, 5)),
-                s.sortThird(new ArrayList<>(Arrays.asList(5, 6, 9, 4, 8, 3, 2))).equals(Arrays.asList(2, 6, 9, 4, 8, 3, 5)),
-                s.sortThird(new ArrayList<>(Arrays.asList(5, 6, 3, 4, 8, 9, 2, 1))).equals(Arrays.asList(2, 6, 3, 4, 8, 9, 5, 1))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testSortThird_listWithLastIndexDivisibleByThree0_sortsOnlyIndicesDivisibleByThree() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 6, 3, 4, 8, 9, 2);
+        List<Integer> expectedSortedElements = Arrays.asList(2, 6, 3, 4, 8, 9, 5);
+        List<Integer> actualSortedElements = task.sortThird(l);
+        assertEquals(expectedSortedElements, actualSortedElements);
+    }
+
+    @Test
+    public void testSortThird_listWithLastIndexDivisibleByThree1_sortsOnlyIndicesDivisibleByThree() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 8, 3, 4, 6, 9, 2);
+        List<Integer> expectedSortedElements = Arrays.asList(2, 8, 3, 4, 6, 9, 5);
+        List<Integer> actualSortedElements = task.sortThird(l);
+        assertEquals(expectedSortedElements, actualSortedElements);
+    }
+
+    @Test
+    public void testSortThird_listWithLastIndexDivisibleByThree2_sortsOnlyIndicesDivisibleByThree() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 6, 9, 4, 8, 3, 2);
+        List<Integer> expectedSortedElements = Arrays.asList(2, 6, 9, 4, 8, 3, 5);
+        List<Integer> actualSortedElements = task.sortThird(l);
+        assertEquals(expectedSortedElements, actualSortedElements);
+    }
+
+    @Test
+    public void testSortThird_listWithLastIndexNotDivisibleByThree_sortsOnlyIndicesDivisibleByThree() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 6, 3, 4, 8, 9, 2, 1);
+        List<Integer> expectedSortedElements = Arrays.asList(2, 6, 3, 4, 8, 9, 5, 1);
+        List<Integer> actualSortedElements = task.sortThird(l);
+        assertEquals(expectedSortedElements, actualSortedElements);
     }
 }
