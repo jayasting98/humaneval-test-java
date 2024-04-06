2ea5ed13f44c6c921faca1452885e3a782a8d4ef
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 23:29:18 +0800

    Rewrite task 51

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task51/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task51/TaskTest.java
@@ -1,21 +1,72 @@
 package humanevaltest.unit.task51;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                Objects.equals(s.removeVowels(""), ""),
-                Objects.equals(s.removeVowels("abcdef\nghijklm"), "bcdf\nghjklm"),
-                Objects.equals(s.removeVowels("fedcba"), "fdcb"),
-                Objects.equals(s.removeVowels("eeeee"), ""),
-                Objects.equals(s.removeVowels("acBAA"), "cB"),
-                Objects.equals(s.removeVowels("EcBOO"), "cB"),
-                Objects.equals(s.removeVowels("ybcd"), "ybcd")
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testRemoveVowels_emptyString_removesNothing() {
+        Task task = new Task();
+        String text = "";
+        String expectedResult = "";
+        String actualResult = task.removeVowels(text);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testRemoveVowels_textWithVowelsAndNonAlphabet_removesVowels() {
+        Task task = new Task();
+        String text = "abcdef\nghijklm";
+        String expectedResult = "bcdf\nghjklm";
+        String actualResult = task.removeVowels(text);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testRemoveVowels_textWithVowelsInLowerCase_removesVowels() {
+        Task task = new Task();
+        String text = "fedcba";
+        String expectedResult = "fdcb";
+        String actualResult = task.removeVowels(text);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testRemoveVowels_textWithOnlyVowels_removesEverything() {
+        Task task = new Task();
+        String text = "eeeee";
+        String expectedResult = "";
+        String actualResult = task.removeVowels(text);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testRemoveVowels_textWithVowelsInBothCases_removesVowels() {
+        Task task = new Task();
+        String text = "acBAA";
+        String expectedResult = "cB";
+        String actualResult = task.removeVowels(text);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testRemoveVowels_textWithVowelsInUpperCase_removesVowels() {
+        Task task = new Task();
+        String text = "EcBOO";
+        String expectedResult = "cB";
+        String actualResult = task.removeVowels(text);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testRemoveVowels_textWithNoVowels_removesVowels() {
+        Task task = new Task();
+        String text = "ybcd";
+        String expectedResult = "ybcd";
+        String actualResult = task.removeVowels(text);
+        assertEquals(expectedResult, actualResult);
     }
 }
