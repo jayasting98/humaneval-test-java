4a252c742886dbbb9620850f1025e0f370fac090
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 14:38:02 +0800

    Rewrite task 34

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task34/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task34/TaskTest.java
@@ -1,15 +1,18 @@
 package humanevaltest.unit.task34;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.unique(new ArrayList<>(Arrays.asList(5, 3, 5, 2, 3, 3, 9, 0, 123))).equals(Arrays.asList(0, 2, 3, 5, 9, 123))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testUnique_typicalNumbers_removesDuplicates() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 3, 5, 2, 3, 3, 9, 0, 123);
+        List<Integer> expectedUniqueNumbers = Arrays.asList(0, 2, 3, 5, 9, 123);
+        List<Integer> actualUniqueNumbers = task.unique(l);
+        assertEquals(expectedUniqueNumbers, actualUniqueNumbers);
     }
 }
