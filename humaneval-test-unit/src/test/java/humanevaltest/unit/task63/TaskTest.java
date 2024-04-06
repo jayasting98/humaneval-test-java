6b70bc15966ac27ab3d36de791b952000a4e8fa0
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-07 02:41:14 +0800

    Rewrite task 63

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task63/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task63/TaskTest.java
@@ -1,21 +1,72 @@
 package humanevaltest.unit.task63;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.fibfib(2) == 1,
-                s.fibfib(1) == 0,
-                s.fibfib(5) == 4,
-                s.fibfib(8) == 24,
-                s.fibfib(10) == 81,
-                s.fibfib(12) == 274,
-                s.fibfib(14) == 927
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testFibfib_nEqualsTwo_calculates1() {
+        Task task = new Task();
+        int n = 2;
+        int expectedValue = 1;
+        int actualValue = task.fibfib(n);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testFibfib_nEqualsOne_calculates0() {
+        Task task = new Task();
+        int n = 1;
+        int expectedValue = 0;
+        int actualValue = task.fibfib(n);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testFibfib_nGreaterThanTwo0_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 5;
+        int expectedValue = 4;
+        int actualValue = task.fibfib(n);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testFibfib_nGreaterThanTwo1_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 8;
+        int expectedValue = 24;
+        int actualValue = task.fibfib(n);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testFibfib_nGreaterThanTwo2_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 10;
+        int expectedValue = 81;
+        int actualValue = task.fibfib(n);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testFibfib_nGreaterThanTwo3_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 12;
+        int expectedValue = 274;
+        int actualValue = task.fibfib(n);
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void testFibfib_nGreaterThanTwo4_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 14;
+        int expectedValue = 927;
+        int actualValue = task.fibfib(n);
+        assertEquals(expectedValue, actualValue);
     }
 }
