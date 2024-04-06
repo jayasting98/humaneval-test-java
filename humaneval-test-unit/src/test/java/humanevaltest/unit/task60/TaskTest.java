86ffdee2f7eb340e7e32dfd882cf65ab5cd20442
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-07 02:20:12 +0800

    Rewrite task 60

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task60/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task60/TaskTest.java
@@ -1,19 +1,54 @@
 package humanevaltest.unit.task60;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.sumToN(1) == 1,
-                s.sumToN(6) == 21,
-                s.sumToN(11) == 66,
-                s.sumToN(30) == 465,
-                s.sumToN(100) == 5050
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testSumToN_nGreaterThanZero0_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 1;
+        int expectedSum = 1;
+        int actualSum = task.sumToN(n);
+        assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void testSumToN_nGreaterThanZero1_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 6;
+        int expectedSum = 21;
+        int actualSum = task.sumToN(n);
+        assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void testSumToN_nGreaterThanZero2_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 11;
+        int expectedSum = 66;
+        int actualSum = task.sumToN(n);
+        assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void testSumToN_nGreaterThanZero3_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 30;
+        int expectedSum = 465;
+        int actualSum = task.sumToN(n);
+        assertEquals(expectedSum, actualSum);
+    }
+
+    @Test
+    public void testSumToN_nGreaterThanZero4_calculatesCorrectly() {
+        Task task = new Task();
+        int n = 100;
+        int expectedSum = 5050;
+        int actualSum = task.sumToN(n);
+        assertEquals(expectedSum, actualSum);
     }
 }
