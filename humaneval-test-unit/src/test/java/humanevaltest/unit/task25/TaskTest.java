81096ba87add1a321ff4081d41fa6a270e592ca4
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 13:32:39 +0800

    Rewrite task 25

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task25/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task25/TaskTest.java
@@ -1,22 +1,81 @@
 package humanevaltest.unit.task25;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.factorize(2).equals(List.of(2)),
-                s.factorize(4).equals(Arrays.asList(2, 2)),
-                s.factorize(8).equals(Arrays.asList(2, 2, 2)),
-                s.factorize(3 * 19).equals(Arrays.asList(3, 19)),
-                s.factorize(3 * 19 * 3 * 19).equals(Arrays.asList(3, 3, 19, 19)),
-                s.factorize(3 * 19 * 3 * 19 * 3 * 19).equals(Arrays.asList(3, 3, 3, 19, 19, 19)),
-                s.factorize(3 * 19 * 19 * 19).equals(Arrays.asList(3, 19, 19, 19)),
-                s.factorize(3 * 2 * 3).equals(Arrays.asList(2, 3, 3))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testFactorize_prime_returnsOnlyPrime() {
+        Task task = new Task();
+        int n = 2;
+        List<Integer> expectedFactors = List.of(2);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
+    }
+
+    @Test
+    public void testFactorize_squaredPrime_returnsPrimeTwice() {
+        Task task = new Task();
+        int n = 4;
+        List<Integer> expectedFactors = Arrays.asList(2, 2);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
+    }
+
+    @Test
+    public void testFactorize_cubedPrime_returnsPrimeThrice() {
+        Task task = new Task();
+        int n = 8;
+        List<Integer> expectedFactors = Arrays.asList(2, 2, 2);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
+    }
+
+    @Test
+    public void testFactorize_productOfPrimes_returnsEachPrime() {
+        Task task = new Task();
+        int n = 3 * 19;
+        List<Integer> expectedFactors = Arrays.asList(3, 19);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
+    }
+
+    @Test
+    public void testFactorize_squaredProductOfPrimes_returnsEachPrimeTwice() {
+        Task task = new Task();
+        int n = 3 * 19 * 3 * 19;
+        List<Integer> expectedFactors = Arrays.asList(3, 3, 19, 19);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
+    }
+
+    @Test
+    public void testFactorize_cubedProductOfPrimes_returnsEachPrimeThrice() {
+        Task task = new Task();
+        int n = 3 * 19 * 3 * 19 * 3 * 19;
+        List<Integer> expectedFactors = Arrays.asList( 3, 3, 3, 19, 19, 19);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
+    }
+
+    @Test
+    public void testFactorize_primeProduct0_factorizesCorrectly() {
+        Task task = new Task();
+        int n = 3 * 19 * 19 * 19;
+        List<Integer> expectedFactors = Arrays.asList(3, 19, 19, 19);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
+    }
+
+    @Test
+    public void testFactorize_primeProduct1_factorizesCorrectly() {
+        Task task = new Task();
+        int n = 3 * 2 * 3;
+        List<Integer> expectedFactors = Arrays.asList(2, 3, 3);
+        List<Integer> actualFactors = task.factorize(n);
+        assertEquals(expectedFactors, actualFactors);
     }
 }
