736ccf7aae2e468f4e6ab6b33bc92200665ef400
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 13:52:55 +0800

    Rewrite task 29

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task29/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task29/TaskTest.java
@@ -1,16 +1,29 @@
 package humanevaltest.unit.task29;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.filterByPrefix(new ArrayList<>(List.of()), "john").equals(List.of()),
-                s.filterByPrefix(new ArrayList<>(Arrays.asList("xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx")), "xxx").equals(Arrays.asList("xxx", "xxxAAA", "xxx"))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testFilterByPrefix_emptyList_returnsEmptyList() {
+        Task task = new Task();
+        List<String> strings = List.of();
+        String prefix = "john";
+        List<String> expectedFilteredStrings = List.of();
+        List<String> actualFilteredStrings = task.filterByPrefix(strings, prefix);
+        assertEquals(expectedFilteredStrings, actualFilteredStrings);
+    }
+
+    @Test
+    public void testFilterByPrefix_typicalString_returnsOnlyStringsWithPrefix() {
+        Task task = new Task();
+        List<String> strings = Arrays.asList("xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx");
+        String prefix = "xxx";
+        List<String> expectedFilteredStrings = Arrays.asList("xxx", "xxxAAA", "xxx");
+        List<String> actualFilteredStrings = task.filterByPrefix(strings, prefix);
+        assertEquals(expectedFilteredStrings, actualFilteredStrings);
     }
 }
