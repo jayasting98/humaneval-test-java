baa8fc89f360430a92d0bde2b92f9adde34e4b13
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 14:02:14 +0800

    Rewrite task 30

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task30/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task30/TaskTest.java
@@ -1,18 +1,45 @@
 package humanevaltest.unit.task30;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.getPositive(new ArrayList<>(Arrays.asList(-1, -2, 4, 5, 6))).equals(Arrays.asList(4, 5, 6)),
-                s.getPositive(new ArrayList<>(Arrays.asList(5, 3, -5, 2, 3, 3, 9, 0, 123, 1, -10))).equals(Arrays.asList(5, 3, 2, 3, 3, 9, 123, 1)),
-                s.getPositive(new ArrayList<>(Arrays.asList(-1, -2))).equals(List.of()),
-                s.getPositive(List.of()).equals(List.of())
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testGetPositive_typicalNumbers0_getsPositiveNumbersOnly() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(-1, -2, 4, 5, 6);
+        List<Integer> expectedPositiveNumbers = Arrays.asList(4, 5, 6);
+        List<Integer> actualPositiveNumbers = task.getPositive(l);
+        assertEquals(expectedPositiveNumbers, actualPositiveNumbers);
+    }
+
+    @Test
+    public void testGetPositive_typicalNumbers1_getsPositiveNumbersOnly() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 3, -5, 2, 3, 3, 9, 0, 123, 1, -10);
+        List<Integer> expectedPositiveNumbers = Arrays.asList(5, 3, 2, 3, 3, 9, 123, 1);
+        List<Integer> actualPositiveNumbers = task.getPositive(l);
+        assertEquals(expectedPositiveNumbers, actualPositiveNumbers);
+    }
+
+    @Test
+    public void testGetPositive_onlyNegativeNumbers_getsNothing() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(-1, -2);
+        List<Integer> expectedPositiveNumbers = List.of();
+        List<Integer> actualPositiveNumbers = task.getPositive(l);
+        assertEquals(expectedPositiveNumbers, actualPositiveNumbers);
+    }
+
+    @Test
+    public void testGetPositive_emptyList_getsNothing() {
+        Task task = new Task();
+        List<Integer> l = List.of();
+        List<Integer> expectedPositiveNumbers = List.of();
+        List<Integer> actualPositiveNumbers = task.getPositive(l);
+        assertEquals(expectedPositiveNumbers, actualPositiveNumbers);
     }
 }
