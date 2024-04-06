145826a966b85b2fc81f112c8b5175b9a23ecf96
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 19:50:00 +0800

    Rewrite task 42

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task42/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task42/TaskTest.java
@@ -1,17 +1,36 @@
 package humanevaltest.unit.task42;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.incrList(new ArrayList<>(Arrays.asList())).equals(List.of()),
-                s.incrList(new ArrayList<>(Arrays.asList(3, 2, 1))).equals(Arrays.asList(4, 3, 2)),
-                s.incrList(new ArrayList<>(Arrays.asList(5, 2, 5, 2, 3, 3, 9, 0, 123))).equals(Arrays.asList(6, 3, 6, 3, 4, 4, 10, 1, 124))
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testIncrList_emptyList_incrementsNothing() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList();
+        List<Integer> expectedIncrementedNumbers = List.of();
+        List<Integer> actualIncrementedNumbers = task.incrList(l);
+        assertEquals(expectedIncrementedNumbers, actualIncrementedNumbers);
+    }
+
+    @Test
+    public void testIncrList_typicalNumbers0_incrementsAll() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(3, 2, 1);
+        List<Integer> expectedIncrementedNumbers = Arrays.asList(4, 3, 2);
+        List<Integer> actualIncrementedNumbers = task.incrList(l);
+        assertEquals(expectedIncrementedNumbers, actualIncrementedNumbers);
+    }
+
+    @Test
+    public void testIncrList_typicalNumbers1_incrementsAll() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 2, 5, 2, 3, 3, 9, 0, 123);
+        List<Integer> expectedIncrementedNumbers = Arrays.asList(6, 3, 6, 3, 4, 4, 10, 1, 124);
+        List<Integer> actualIncrementedNumbers = task.incrList(l);
+        assertEquals(expectedIncrementedNumbers, actualIncrementedNumbers);
     }
 }
