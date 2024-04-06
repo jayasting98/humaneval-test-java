84d9cee9efac5e1919215cd9f9008bcd55ffdbec
Jay Aljelo Ting <65202977+jayasting98@users.noreply.github.com>
2024-04-06 19:09:49 +0800

    Rewrite task 35

--- humaneval-test-unit/src/test/java/humanevaltest/unit/task35/TaskTest.java
+++ humaneval-test-unit/src/test/java/humanevaltest/unit/task35/TaskTest.java
@@ -1,16 +1,27 @@
 package humanevaltest.unit.task35;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.*;
 
+import org.junit.jupiter.api.Test;
+
 public class TaskTest {
-    public static void main(String[] args) {
-        Task s = new Task();
-        List<Boolean> correct = Arrays.asList(
-                s.maxElement(new ArrayList<>(Arrays.asList(1, 2, 3))) == 3,
-                s.maxElement(new ArrayList<>(Arrays.asList(5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10))) == 124
-        );
-        if (correct.contains(false)) {
-            throw new AssertionError();
-        }
+    @Test
+    public void testMaxElement_increasingList_returnsLastElement() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(1, 2, 3);
+        int expectedMax = 3;
+        int actualMax = task.maxElement(l);
+        assertEquals(expectedMax, actualMax);
+    }
+
+    @Test
+    public void testMaxElement_unorderedList_returnsMax() {
+        Task task = new Task();
+        List<Integer> l = Arrays.asList(5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10);
+        int expectedMax = 124;
+        int actualMax = task.maxElement(l);
+        assertEquals(expectedMax, actualMax);
     }
 }
